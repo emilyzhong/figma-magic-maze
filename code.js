293d5463a1f9cff6cac30459baa790c6b3b133d4
Emily Zhong <emily.zhong@berkeley.edu>
2020-12-02 12:22:39 -0800

    Add in timer-updating logic

--- code.js
+++ code.js
@@ -4,7 +4,7 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { height: 300, width: 300 });
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
@@ -52,6 +52,10 @@ figma.ui.onmessage = msg => {
         case 'generate-tiles':
             generateTiles(msg.numPlayers);
             break;
+        case 'start-game':
+            // TODO: Add other game-starting logic.
+            setInterval(updateTimer, 1000);
+            break;
     }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
@@ -65,7 +69,8 @@ var Directions;
     Directions[Directions["RIGHT"] = 3] = "RIGHT";
 })(Directions || (Directions = {}));
 const gameState = {
-    allowedDirections: []
+    allowedDirections: [],
+    timerInterval: null,
 };
 const checkValidDirection = (direction) => {
     return gameState.allowedDirections.indexOf(direction) !== -1;
@@ -73,13 +78,24 @@ const checkValidDirection = (direction) => {
 const generateTiles = (numPlayers) => {
     // TODO: Generate the right move cards for the number of players
 };
+const updateTimer = () => {
+    updateTimerInSeconds(getTimerInSeconds() - 1);
+};
+// TODO: Use this method when a player gets placed on a timer tile.
 const flipTimer = () => {
+    const currRemainingTime = getTimerInSeconds();
+    const flippedRemainingTime = 60 * 3 - currRemainingTime;
+    updateTimerInSeconds(flippedRemainingTime);
+};
+const getTimerInSeconds = () => {
     const timerEl = figma.currentPage.findOne((node) => node.name === 'Timer' && node.type === 'TEXT');
     const timeTokens = timerEl.characters.split(':');
-    const currRemainingTime = parseInt(timeTokens[0]) * 60 + parseInt(timeTokens[1]);
-    const flippedRemainingTime = 60 * 3 - currRemainingTime;
-    const minutes = Math.floor(flippedRemainingTime / 60);
-    const seconds = flippedRemainingTime - minutes * 60;
+    return parseInt(timeTokens[0]) * 60 + parseInt(timeTokens[1]);
+};
+const updateTimerInSeconds = (timerInSeconds) => {
+    const timerEl = figma.currentPage.findOne((node) => node.name === 'Timer' && node.type === 'TEXT');
+    const minutes = Math.floor(timerInSeconds / 60);
+    const seconds = timerInSeconds - minutes * 60;
     const newTime = `${minutes}:${(`0${seconds}`).slice(-2)}`;
     figma.loadFontAsync(timerEl.fontName).then(() => {
         timerEl.deleteCharacters(0, timerEl.characters.length);
--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { height: 300, width: 300 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
@@ -57,6 +57,11 @@ figma.ui.onmessage = msg => {
     case 'generate-tiles':
       generateTiles(msg.numPlayers)
       break
+
+    case 'start-game':
+      // TODO: Add other game-starting logic.
+      setInterval(updateTimer, 1000)
+      break
   }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
@@ -73,11 +78,13 @@ enum Directions {
 
 type GameState = {
   allowedDirections: Directions[],
+  timerInterval: () => void | null,
   // TODO: Add more game-state specific values here.
 }
 
 const gameState: GameState = {
-  allowedDirections: []
+  allowedDirections: [],
+  timerInterval: null,
 }
 
 const checkValidDirection = (direction: Directions) => {
@@ -88,15 +95,29 @@ const generateTiles = (numPlayers: number) => {
   // TODO: Generate the right move cards for the number of players
 }
 
+const updateTimer = () => {
+  updateTimerInSeconds(getTimerInSeconds() - 1)
+}
+
+// TODO: Use this method when a player gets placed on a timer tile.
 const flipTimer = () => {
+  const currRemainingTime = getTimerInSeconds()
+  const flippedRemainingTime = 60 * 3 - currRemainingTime
+  updateTimerInSeconds(flippedRemainingTime)
+}
+
+const getTimerInSeconds = () => {
   const timerEl = figma.currentPage.findOne((node) => node.name === 'Timer' && node.type === 'TEXT') as TextNode
   const timeTokens = timerEl.characters.split(':')
 
-  const currRemainingTime = parseInt(timeTokens[0]) * 60 + parseInt(timeTokens[1])
-  const flippedRemainingTime = 60 * 3 - currRemainingTime
+  return parseInt(timeTokens[0]) * 60 + parseInt(timeTokens[1])
+}
 
-  const minutes = Math.floor(flippedRemainingTime / 60)
-  const seconds = flippedRemainingTime - minutes * 60
+const updateTimerInSeconds = (timerInSeconds: number) => {
+  const timerEl = figma.currentPage.findOne((node) => node.name === 'Timer' && node.type === 'TEXT') as TextNode
+
+  const minutes = Math.floor(timerInSeconds / 60)
+  const seconds = timerInSeconds - minutes * 60
   const newTime = `${minutes}:${(`0${seconds}`).slice(-2)}`
 
   figma.loadFontAsync(timerEl.fontName as FontName).then(() => {
